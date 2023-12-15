057a03f4057f5a7725e3b7415ff93b2f4c63dc50
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2023-12-15 13:33:40 -0500

    completed user api

--- src/main/java/com/vikram/blogapp/controller/UserController.java
+++ src/main/java/com/vikram/blogapp/controller/UserController.java
@@ -5,10 +5,9 @@ import com.vikram.blogapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,5 +20,33 @@ public class UserController {
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
         UserDTO createdUser = userService.createUser(userDTO);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    // PUT - Update User
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO, @PathVariable long userId){
+        UserDTO updatedUser = userService.updateUser(userDTO, userId);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    // DELETE - Delete User
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable long userId){
+        UserDTO deletedUser = userService.deleteUser(userId);
+        return new ResponseEntity<>(deletedUser, HttpStatus.OK);
+    }
+
+    // GET - User by ID
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable long userId){
+        UserDTO user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // GET - Get all users
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        List<UserDTO> allUsers = userService.getAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
     }
 }
