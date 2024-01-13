ce378ba5903afed456ad9c6b3491bd0952c2fb01
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-01-13 17:34:50 -0500

    documented all the apis

--- src/main/java/com/vikram/blogapp/controller/AuthController.java
+++ src/main/java/com/vikram/blogapp/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.vikram.blogapp.controller;
 
-import com.vikram.blogapp.dto.AuthRequest;
-import com.vikram.blogapp.dto.AuthResponse;
-import com.vikram.blogapp.dto.RegisterUserRequest;
+import com.vikram.blogapp.dto.*;
 import com.vikram.blogapp.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,18 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
+    @Operation(summary = "Registers a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User is registered",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponse.class)) }),
+            @ApiResponse(responseCode = "409", description = "User already exists",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Request body is invalid",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<AuthResponse> register(
             @RequestBody @Valid RegisterUserRequest request
     ) {
@@ -27,6 +42,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Log in a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User is logged in",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Request body is invalid",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<AuthResponse> login(
             @RequestBody @Valid AuthRequest request
     ) {
--- src/main/java/com/vikram/blogapp/controller/CategoryController.java
+++ src/main/java/com/vikram/blogapp/controller/CategoryController.java
@@ -1,8 +1,15 @@
 package com.vikram.blogapp.controller;
 
 import com.vikram.blogapp.dto.CategoryDTO;
+import com.vikram.blogapp.dto.ErrorResponse;
 import com.vikram.blogapp.dto.PostDTO;
 import com.vikram.blogapp.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,42 +24,106 @@ import java.util.List;
 public class CategoryController {
     private final CategoryService categoryService;
 
-    // POST - Create Category
     @PostMapping
+    @Operation(summary = "Create a Category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Created the category",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CategoryDTO.class)) }),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Request body is invalid",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody @Valid CategoryDTO categoryDTO){
         CategoryDTO createdCategory = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
     }
 
-    // PUT - Update Category
     @PutMapping("/{categoryId}")
+    @Operation(summary = "Update a category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated the category",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CategoryDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Category not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Request body is invalid",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<CategoryDTO> updateCategory(@RequestBody @Valid CategoryDTO categoryDTO, @PathVariable long categoryId){
         CategoryDTO updatedCategory = categoryService.updateCategory(categoryDTO, categoryId);
         return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 
-    // DELETE - Delete Category
     @DeleteMapping("/{categoryId}")
+    @Operation(summary = "Delete a category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted the category",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CategoryDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Category not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable long categoryId){
         CategoryDTO deletedCategory = categoryService.deleteCategory(categoryId);
         return new ResponseEntity<>(deletedCategory, HttpStatus.OK);
     }
 
-    // GET - Category by ID
     @GetMapping("/{categoryId}")
+    @Operation(summary = "Get a category by categoryId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns the category",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CategoryDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Category not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<CategoryDTO> getCategory(@PathVariable long categoryId){
         CategoryDTO category = categoryService.getCategory(categoryId);
         return new ResponseEntity<>(category, HttpStatus.OK);
     }
 
-    // GET - Get all categories
     @GetMapping
+    @Operation(summary = "Get all categories")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all the categories",
+                    content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CategoryDTO.class)))}),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     public ResponseEntity<List<CategoryDTO>> getAllCategories(){
         List<CategoryDTO> allCategories = categoryService.getAllCategories();
         return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
-    // GET - Get all posts in a category
+    @Operation(summary = "Get all posts in a category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all the posts in the category",
+                    content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PostDTO.class)))}),
+            @ApiResponse(responseCode = "403", description = "User not authorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Category not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))}
+    )
     @GetMapping("/{categoryId}/posts")
     public ResponseEntity<List<PostDTO>> getAllPostsByCategory(@PathVariable long categoryId){
         List<PostDTO> allPosts = categoryService.getAllPostsByCategory(categoryId);
